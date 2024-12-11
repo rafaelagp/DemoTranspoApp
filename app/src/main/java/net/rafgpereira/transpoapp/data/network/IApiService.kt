46bb8e71c2d437c2b32247e57432fa101c7d07ee
Rafael Pereira <rafael.agp@globo.com>
2024-12-11 19:24:34 -0300

    Partially fixed getHistory request

--- app/src/main/java/net/rafgpereira/transpoapp/data/network/IApiService.kt
+++ app/src/main/java/net/rafgpereira/transpoapp/data/network/IApiService.kt
@@ -11,6 +11,7 @@ import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface IApiService {
 
@@ -20,10 +21,10 @@ interface IApiService {
     @PATCH("/ride/confirm")
     suspend fun confirm(@Body requestBody: ConfirmRequestBody) : Response<ConfirmResult>
 
-    @GET("/ride/{userId}?driver_id={driverId}")
+    @GET("/ride/{userId}")
     suspend fun getHistory(
         @Path("userId") userId: String,
-        @Path("driverId") driverId: Int
+        @Query("driver_id") driverId: Int
     ) : Response<HistoryResult>
 
     companion object {
--- app/src/main/java/net/rafgpereira/transpoapp/domain/model/Ride.kt
+++ app/src/main/java/net/rafgpereira/transpoapp/domain/model/Ride.kt
@@ -1,12 +1,12 @@
 package net.rafgpereira.transpoapp.domain.model
 
 data class Ride(
-    val id: Long,
+    val id: Int,
     val date: String,
     val origin: String,
     val destination: String,
-    val distance: Int,
-    val duration: Int,
+    val distance: Double,
+    val duration: String,
     val driver: Driver,
     val value: Double,
 )
